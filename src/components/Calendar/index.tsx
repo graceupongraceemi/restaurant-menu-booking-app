1fa5f2752de8e59b5bfdc9ff4d865e5c61eb3190
Emiko Esther S <graceupongraceemi@gmail.com>
2023-04-26 22:50:02 -0700

    initial commit. components directory created and Calender folder added and index.tsx file created

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+import ReactCalendar from 'react-calendar';
+
+interface indexProps {}
+
+const index: FC<indexProps> = () => {
+  return (
+    <div>
+      <ReactCalendar />
+    </div>
+  );
+};
+
+export default index;
