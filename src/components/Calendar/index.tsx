a9fcdbc2f906f5da86a2df99f12b44ebb09e7a8d
Emiko Esther S <graceupongraceemi@gmail.com>
2023-05-19 00:16:50 -0700

    interface DateType added and also for useState in index.tsx of Calendar folder

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,11 +1,20 @@
-import type { FC } from 'react';
+import { FC, useState } from 'react';
 import ReactCalendar from 'react-calendar';
 
 interface indexProps {}
 
+interface DateType {
+  justDate: Date | null;
+  dateTime: Date | null;
+}
+
 const index: FC<indexProps> = () => {
+  const [date, setDate] = useState<DateType>({
+    justDate: null,
+    dateTime: null
+  });
   return (
-    <div>
+    <div className='flex h-screen flex-col justify-center'>
       <ReactCalendar
         minDate={new Date()}
         className='REACT-CALENDAR P-2'
