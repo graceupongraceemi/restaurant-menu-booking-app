5200be5069c4c7617431babb6a64dbb9084080cc
Emiko Esther S <graceupongraceemi@gmail.com>
2023-05-21 10:52:44 -0700

    trpc.ts file modified. As well, context.ts added

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -1,6 +1,5 @@
 import { initTRPC, TRPCError } from '@trpc/server';
 import superjson from 'superjson';
-import { verifyAuth } from '../../lib/auth';
 
 import { type Context } from './context';
 
@@ -11,26 +10,6 @@ const t = initTRPC.context<Context>().create({
   }
 });
 
-const isAdmin = t.middleware(async ({ ctx, next }) => {
-  const { req } = ctx;
-  const token = req.cookies['user-token'];
-
-  if (!token) {
-    throw new TRPCError({
-      code: 'UNAUTHORIZED',
-      message: 'Missing user token'
-    });
-  }
-  const verifiedToken = await verifyAuth(token);
-
-  if (!verifiedToken) {
-    throw new TRPCError({
-      code: 'UNAUTHORIZED',
-      message: 'Invalid user token'
-    });
-  }
-
-  return next();
-});
-
 export const router = t.router;
+
+export const publicProcedure = t.procedure;
