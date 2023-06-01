c50548b915692f05e672bfd9286d83d2d2e198f3
Emiko Esther S <graceupongraceemi@gmail.com>
2023-05-31 20:31:51 -0700

    index.tsx in dashboard modified by adding Opening hours and Menu links

--- src/pages/dashbord/index.tsx
+++ src/pages/dashbord/index.tsx
@@ -1,17 +1,18 @@
 import { FC } from 'react';
 import { trpc } from '../../utils/trpc';
+import Link from 'next/link';
 
 interface dashboardProps {}
 
 const dashboard: FC<dashboardProps> = ({}) => {
-  const { mutate } = trpc.admin.sensitive.useMutation();
-
   return (
-    <div>
-      dashboard{' '}
-      <button type='button' onClick={() => mutate()}>
-        TOP SECRET ACTION
-      </button>
+    <div className='flex h-screen w-full items-center justify-center gap-8 font-medium'>
+      <Link className='rounded-md bg-gray-100 p-2' href='/dashboard/opening'>
+        Opening hours
+      </Link>
+      <Link className='rounded-md bg-gray-100 p-2' href='/dashboard/menu'>
+        Menu
+      </Link>
     </div>
   );
 };
