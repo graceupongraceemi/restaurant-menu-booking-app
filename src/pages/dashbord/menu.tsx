3fdc60fc4dd38b8cfa5e97104d1abfb77dd42642
Emiko Esther S <graceupongraceemi@gmail.com>
2023-06-01 22:18:35 -0700

    In menu function, input name and price as well as DynamicSelect added

--- src/pages/dashbord/menu.tsx
+++ src/pages/dashbord/menu.tsx
@@ -3,7 +3,44 @@ import { FC } from 'react';
 interface menuProps {}
 
 const menu: FC<menuProps> = ({}) => {
-  return <div>menu</div>;
+  return (
+    <>
+      <div className=''>
+        <div className='mx-auto flex max-w-xl flex-col gap-2'>
+          <input
+            name='name'
+            className='border-non bg-grey-200 h-12 rounded-sm'
+            type='text'
+            placeholder='name'
+            onChange={handleTextChange}
+            value={input.name}
+          />
+
+          <input
+            name='price'
+            className='border-non bg-grey-200 h-12 rounded-sm'
+            type='number'
+            placeholder='price'
+            onChange={(e) =>
+              setInput((prev) => ({ ...prev, price: Number(e.target.value) }))
+            }
+            value={input.price}
+          />
+
+          <DynamicSelect
+            value={input.categories}
+            // @ts-expect-error when using dynamic import, typescript doesn't know about the
+            onChange={(e) => setInput((prev) => ({ ...prev, categories: e }))}
+            isMulti
+            className='h-12'
+            options={selectOptions}
+          />
+
+          <label htmlFor='file'></label>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default menu;
