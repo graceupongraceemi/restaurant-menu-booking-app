c7c6b80c6e1a3692f8ee5152adc95ab5a6d59826
Emiko Esther S <graceupongraceemi@gmail.com>
2023-06-03 22:36:16 -0700

    helpers.ts file created in utils

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,3 +1,5 @@
 export const STORE_OPENING_TIME = 9; // in hours
 export const STORE_CLOSING_TIME = 12; // in hours
 export const INTERVAL = 30; // in minutes
+
+export const categories = ['all', 'breakfast', 'lunch', 'dinner'] as const;
--- src/pages/dashbord/menu.tsx
+++ src/pages/dashbord/menu.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { selectOptions } from '~/utils/helpers';
 
 interface menuProps {}
 
