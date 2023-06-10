6a3ce26ab93105ec0506aa36b03f6c87f6bba5da
Emiko Esther S <graceupongraceemi@gmail.com>
2023-06-09 17:58:04 -0700

    admin.ts in router modified with addMenuItem and deleteMenuItem

--- src/server/trpc/router/admin.ts
+++ src/server/trpc/router/admin.ts
@@ -49,16 +49,59 @@ export const adminRouter = router({
       const ex = input.fileType.split('/')[1];
       const key = `${id}.${ex}`;
 
-      const { url, fields } = await new Promise((resolve, reject) => {
-        s3.createPresignedPost({
-          Bucket: 'restaurant-booking-software',
-          Fields: { key },
-          Expires: 60,
-          Conditions: [
-            ['content-length-range', 0, MAX_FILE_SIZE],
-            ['starts-with', '$Content-Type', 'image/']
-          ]
-        });
-      });
+      const { url, fields } = (await new Promise((resolve, reject) => {
+        s3.createPresignedPost(
+          {
+            Bucket: 'restaurant-booking-software',
+            Fields: { key },
+            Expires: 60,
+            Conditions: [
+              ['content-length-range', 0, MAX_FILE_SIZE],
+              ['starts-with', '$Content-Type', 'image/']
+            ]
+          },
+          (err, data) => {
+            if (err) return reject(err);
+            resolve(signed);
+          }
+        );
+      })) as any as { url: string; fields: any };
+
+      return { url, fields, key };
     })
+
+    addMenuItem: adminProcedure.input(z.object({
+      name: z.string(),
+      price: z.number(),
+      imageKey: z.string(),
+      categories: z.array(z.union([z.literal('breakfast'), z.literal('lunch'), z.literal('dinner')])),
+    })
+    
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { imageKey, name, categories, price } = input
+      const menuItem = await ctx.prisma.menuItem.create({
+        data: {
+          imageKey,
+          name,
+          categories,
+          price,
+        },
+      })
+
+      return menuItem
+    }),
+
+    deleteMenuItem: adminProcedure
+    .input(z.object({ imageKey: z.string(), id: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      // Delete image from s3
+      const { imageKey, id } = input
+      await s3.deleteObject({ Bucket: 'restaurant-booking-software', Key: imageKey }).promise()
+
+      // Delete image from db
+      await ctx.prisma.menuItem.delete({ where: { id } })
+
+      return true
+    }),
 });
