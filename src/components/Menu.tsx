3d4c6db637529ffa9988dbd2307832925ef39839
Emiko Esther S <graceupongraceemi@gmail.com>
2023-06-22 20:46:21 -0700

    undefined added to useState

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -8,7 +8,7 @@ interface MenuProps {}
 
 const Menu: FC<MenuProps> = ({}) => {
   const { data: menuItems } = trpc.menu.getMenuItems.useQuery();
-  const [filter, setFilter] = useState<string>('');
+  const [filter, setFilter] = useState<undefined | string>('');
 
   return (
     <div className='bg-white'>
@@ -33,11 +33,11 @@ const Menu: FC<MenuProps> = ({}) => {
         </div>
 
         <div className='mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
-          {filteredMenuItems?.map((menuItem) => (
+          {menuItems?.map((menuItem) => (
             <div key={menuItem.id} className='group relative'>
               <div className='min-h-80 aspect-w-1 aspect-h-1 lg:aspect-none w-full overflow-hidden rounded-md bg-gray-200 hover:opacity-75 lg:h-80'>
                 <div className='relative h-full w-full object-cover object-center lg:h-full lg:w-full'>
-                  <Image
+                  <img
                     src={menuItem.url}
                     alt={menuItem.name}
                     fill
